0b640869dc7f11714090db5a29767e43b1a20643
tmahalsrinivasan <tmahalsrinivasan@hawk.iit.edu>
2022-08-18 15:45:25 -0500

    Human Created

---
+++ src/AnimatedMan.java
@@ -0,0 +1,113 @@
+
+public class AnimatedMan {
+    public static void main(String[] args) throws InterruptedException {
+        while (true){
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+
+
+            System.out.println("     \\ \\ \\ \\ || //                          ");
+            System.out.println("     \\ \\ \\ \\ || //                          ");
+            System.out.println("     \\ \\ \\ \\ || //                          ");
+            System.out.println("      [_#_#_#_#_#_#_]                  ");
+            System.out.println("      |  O    O    |               ");
+            System.out.println("      |     ^      |              ");
+            System.out.println("       |    :     |          ");
+            System.out.println("        |   U    |         ");
+            System.out.println("         |      |                 ");
+            System.out.println("          (    )                ");
+            System.out.println("            ||                 ");
+            System.out.println("      @@    ||                 ");
+            System.out.println("       \\   ||               ");
+            System.out.println("        \\  ||                ");
+            System.out.println("         \\ ||                 ");
+            System.out.println("          \\||                  ");
+            System.out.println("          //||                 ");
+            System.out.println("         // ||                 ");
+            System.out.println("        //  ||                  ");
+            System.out.println("       //   ||                  ");
+            System.out.println("      @@  //  \\                ");
+            System.out.println("         //     \\              ");
+            System.out.println("        //       \\            ");
+            System.out.println("       //          \\             ");
+            System.out.println("        !!!!!$      !!!!!$          ");
+
+
+
+            System.out.println("                               TEJ DEEPAK'S ANIMATED HUMAN                     ");
+
+            Thread.sleep(1000);
+
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+
+            System.out.println("     \\ \\ \\ || //  //                        ");
+            System.out.println("     \\ \\ \\ || // //                        ");
+            System.out.println("     \\ \\ \\ || // //                          ");
+            System.out.println("      [_#_#_#_#_#_#_]                  ");
+            System.out.println("      |  O    O    |               ");
+            System.out.println("      |     ^      |              ");
+            System.out.println("       |    :     |          ");
+            System.out.println("        |   U    |         ");
+            System.out.println("         |      |                 ");
+            System.out.println("          (    )                ");
+            System.out.println("            ||                 ");
+            System.out.println("            ||    @@              ");
+            System.out.println("            ||   //             ");
+            System.out.println("            ||  //               ");
+            System.out.println("            || //               ");
+            System.out.println("            ||//                  ");
+            System.out.println("            || \\             ");
+            System.out.println("            ||   \\              ");
+            System.out.println("            ||     \\             ");
+            System.out.println("            ||       \\           ");
+            System.out.println("          //  \\      @@          ");
+            System.out.println("         //     \\              ");
+            System.out.println("        //       \\            ");
+            System.out.println("       //          \\             ");
+            System.out.println("  !!!!!$        !!!!!$");
+
+
+            Thread.sleep(1000);
+
+
+        }
+
+
+    }
+}
